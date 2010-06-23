6dade8660f40bdc91c4035b9418853acdc19c245
Lysender <dc.eros@gmail.com>
2010-06-23 18:25:37 +0800

    Remove unecessary license text

--- library/Dc/Log.php
+++ library/Dc/Log.php
@@ -1,26 +1,4 @@
 <?php
-
-/**
- * Esperant System Philippines Corp
- *
- * LICENSE
- *
- * All codes that appears on this file is property of
- * Esperant System Philippines Corp
- * All files within this project is also property of ESP.
- * Other third party libraries such as Zend Framework,
- * PEAR, Smarty, Swiftmailer, SimpleTest, PHPUnit and the like
- * are properties of their repective owners.
- * All rights reserved.
- * 
- * Illegal copying, modifications, re-distribution and publishing
- * of the source code without permission from the company
- * is strictly prohibited.
- *
- * @author		Leonel Baer - Sep 25, 2009
- * @copyright	Copyright (c) 2009 Esperant System Philippines Corp (http://esp.ph)
- * @desc		Handles logging for the system activities
- */
  
  class Dc_Log
  {
